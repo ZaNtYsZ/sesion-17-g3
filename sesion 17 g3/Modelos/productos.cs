ef70527b931c200a750a1a47e79be672700e6d8a
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-10-14 07:21:42 -0600

    clase producto

---
+++ sesion 17 g3/Modelos/productos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sesion_17_g3.Modelos
+{
+    internal class productos
+    {
+        ´public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+}
